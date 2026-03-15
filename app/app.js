f20dd12810cb2a4b96c071185e0d53820a8d3180
Shashikant uikey <Goluuk129@gmail.com>
2026-03-15 15:56:00 +0530

    Update message

--- app/app.js
+++ app/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps Project Running 🚀");
+//  res.send("DevOps Project Running 🚀");
+	res.send("DevOps CI/CD Pipeline Working 🚀");
 });
 
 app.listen(3000, () => {
